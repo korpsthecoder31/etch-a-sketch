9dbc511ef07da92226a37c91d22a6ea7f1011e7f
korpsthecoder31 <korpsthewriter@gmail.com>
2024-10-08 08:28:52 +0800

    feat: add mouseover effect + modal for dimensions
    - Change background color of each square to black on mouseover.
    - Add button to open modal.
    - Implement modal with slider to adjust grid dimensions (accept button
      logs value to console).
    - Add cancel button to close modal.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const sketchBox = document.querySelector("#sketchBox")
 
+createGrid(16)
+
 function createGrid(pixelNum) {
 
     for(i = 0; i < pixelNum; i++) {
@@ -16,8 +18,33 @@ function createGrid(pixelNum) {
             column.appendChild(newRow)
         }
     })
-        
 }
 
+const squares = document.querySelectorAll(".row")
 
-createGrid(16)
+squares.forEach(function(square) {
+    square.addEventListener("mouseover", () => 
+        square.setAttribute("id", "black")
+    )
+})
+
+const newGrid = document.querySelector("#changeGridButton")
+newGrid.addEventListener("click", setGrid)
+
+const modal = document.querySelector("#changeGridBox")
+
+function setGrid() {
+    modal.showModal()
+
+    const input = document.querySelector("#gridInput")
+
+    const accept = document.querySelector("#acceptButton")
+    accept.addEventListener("click", () =>
+        console.log(input.value)
+    )
+    
+    const cancel = document.querySelector("#cancelButton")
+    cancel.addEventListener("click", () =>
+        modal.close()
+    )
+}
